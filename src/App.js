49075921cd5519834dacde0dea3fcefa8f3d1d14
Nvejqygroqxk <44037680+Nvejqygroqxk@users.noreply.github.com>
2020-07-23 15:52:48 +0200

    timer + buttons

--- src/App.js
+++ src/App.js
@@ -3,14 +3,34 @@ import './App.css';
 
 function App() {
   const [seconds, setSeconds] = useState(300);
+  const [isActive, setIsActive] = useState(false);
+
+  function toggle() {
+    setIsActive(!isActive);
+  }
+
+  function reset() {
+    setIsActive(false);
+    setSeconds(300);
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds(seconds => seconds - 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+
   return (
     <div className="App">
-      {useEffect(() => {
-        setInterval(() => {
-          setSeconds(seconds => seconds - 1);
-        }, 1000);
-      }, [])
-      }
+      <button onClick={toggle}>{isActive ? 'Pause' : 'Start'}</button>
+      <button onClick={reset}>Reset</button>
       {seconds}
     </div>
   );
