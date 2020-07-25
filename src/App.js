a1f14f1ebe2bff13ba029727b987bc86ca30e40f
Nvejqygroqxk <44037680+Nvejqygroqxk@users.noreply.github.com>
2020-07-25 02:30:43 +0200

    fonctionne else ifs

--- src/App.js
+++ src/App.js
@@ -2,41 +2,62 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [sessionSeconds, setSessionSeconds] = useState(10);
-  const [breakSeconds, setBreakSeconds] =useState(10)
-  const [isActive, setIsActive] = useState(false);
+  const [sessionSeconds, setSessionSeconds] = useState(5);
+  const [breakSeconds, setBreakSeconds] = useState(5)
+  const [isActiveS, setIsActiveS] = useState(false);
+  const [isActiveB, setIsActiveB] = useState(false);
 
-  function toggle() {
-    setIsActive(!isActive);
+  function toggleS() {
+    setIsActiveS(!isActiveS);
   }
-
+/*
+  function toggleB() {
+    setIsActiveB(!isActiveB);
+  }
+*/
   function reset() {
-    setIsActive(false);
-    setSessionSeconds(1500);
+    setIsActiveS(false);
+    setIsActiveB(false);
+    setSessionSeconds(5);
+    setBreakSeconds(5);
   }
 
   useEffect(() => {
     let interval = null;
-    if (isActive && sessionSeconds !== 0) {
+    if (isActiveS && sessionSeconds > -1) {
       interval = setInterval(() => {
         setSessionSeconds(sessionSeconds => sessionSeconds - 1);
-      }, 1000);}
-    else if (isActive && sessionSeconds === 0){
+      }, 1000);
+    }
+    else if (isActiveS && sessionSeconds === -1) {
+      setIsActiveS(false);
+      setSessionSeconds(5);
+      setIsActiveB(true);
+    }
+    else if (isActiveB && breakSeconds > -1) {
       interval = setInterval(() => {
         setBreakSeconds(breakSeconds => breakSeconds - 1);
-      }, 1000);}
-    
-     else if (!isActive && sessionSeconds !== 0) {
+      }, 1000);
+    }
+    else if (isActiveB && breakSeconds === -1) {
+      setIsActiveB(false);
+      setBreakSeconds(5);
+      setIsActiveS(true);
+    }
+    else if (!isActiveS && sessionSeconds !== 0) {
+      clearInterval(interval);
+    }
+    else if (!isActiveB && breakSeconds !== 0) {
       clearInterval(interval);
     }
     return () => clearInterval(interval);
-  }, [isActive, sessionSeconds]);
+  }, [isActiveS, sessionSeconds, isActiveB, breakSeconds]);
 
 
 
   return (
     <div className="App">
-      <button onClick={toggle}>{isActive ? 'Pause' : 'Start'}</button>
+      <button onClick={toggleS}>{isActiveS ? 'Pause' : 'Start'}</button>
       <button onClick={reset}>Reset</button>
       Session : {sessionSeconds}
       Break : {breakSeconds}
